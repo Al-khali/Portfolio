2f53ac0d98fea6a70aa885a35c6623bfe52bc641
Al-khali <aourik.khalid@hotmail.com>
2022-07-14 10:39:22 +0200

    updating project2

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -36,10 +36,10 @@ function Projects() {
             <ProjectCard
               imgPath={bitsOfCode}
               isBlog={false}
-              title="Bits-0f-C0de"
-              description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
-              ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
-              demoLink="https://blogs.soumya-jit.tech/"
+              title="chat_bot"
+              description="a chat bot build with python using NLP algorithm with NLTK framework"
+              ghLink="https://github.com/Al-khali/chat-bot"
+              demoLink="https://al-khali-chat-bot-cbv2-g55mvl.streamlitapp.com/"
             />
           </Col>
 
