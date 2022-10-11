6c499d38a8e81c03f4fd4dcb64c611f67b6021d3
Al-khali <aourik.khalid@hotmail.com>
2022-10-11 15:19:13 +0200

    update

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,14 +2,14 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/cv_aourik_khalid.pdf";
+import pdf from "../../Assets/../Assets/Cv_aourik_khalid.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://raw.githubusercontent.com/Al-khali/portfolio/master/src/Assets/cv_aourik_khalid.pdf";
+  "https://raw.githubusercontent.com/Al-khali/portfolio/master/src/Assets/Cv_aourik_khalid.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
